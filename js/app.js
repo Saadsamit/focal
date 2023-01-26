8870332ef1091f17f66a09636bb3b155838018ac
MD SAMIT INTESAR <104861451+Saadsamit@users.noreply.github.com>
2023-01-26 16:49:54 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,37 @@
+$(function () {
+    $(window).on("scroll", function () {
+        var scrollSize = $(window).scrollTop();
+        //nav section
+        if (scrollSize > 80) {
+          $("#nav").addClass("active");
+        } else {
+          $("#nav").removeClass("active");
+        }
+        if (scrollSize > 80) {
+          $("#side_nav").addClass("active");
+        } else {
+          $("#side_nav").removeClass("active");
+        }
+        //back to top section
+        if (scrollSize > 200) {
+          $("#back_to_top").addClass("active");
+        } else {
+          $("#back_to_top").removeClass("active");
+        }
+      });
+      //back to top section
+      $("#back_to_top").on("click", function () {
+        $("html,body").animate(
+          {
+            scrollTop: 0,
+          },
+          1000
+        );
+      });
+      //review section
+      $('.review_container').slick({
+        arrows: false,
+        dots: true,
+        dotsClass: 'review_dots',
+      });
+})
